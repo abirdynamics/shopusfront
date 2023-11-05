aa57360c25efd178df85e2c4596c1a994eb98540
unknown <Administrator@AJMLHO-WS40.akijassets.com>
2023-11-06 02:29:35 +0600

    google login part 3

--- src/components/Auth/Login/LoginWidget.jsx
+++ src/components/Auth/Login/LoginWidget.jsx
@@ -1,7 +1,8 @@
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+// import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
+import { GoogleLogin } from 'react-google-login';
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import apiRequest from "../../../../utils/apiRequest";
@@ -34,6 +35,7 @@ function LoginWidget({ redirect = true, loginActionPopup, notVerifyHandler }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [googleloading, setGoogleLoading] = useState(false);
   const [checked, setValue] = useState(false);
   const [langCntnt, setLangCntnt] = useState(null);
   const [defaultProfileImg, setDefault] = useState(null);
@@ -130,14 +132,17 @@ function LoginWidget({ redirect = true, loginActionPopup, notVerifyHandler }) {
 
   const socialLogin = async (response) => {
     let token = response.credential;
-    setLoading(true);
+    console.log('token', token)
+    // return
+    // setLoading(true);
     await apiRequest
       .socialLogin({
         token: token
       })
       .then((res) => {
         console.log('res', res)
-        setLoading(false);
+        return
+        // setLoading(false);
         toast.success(langCntnt && langCntnt.Login_Successfully);
         setEmail("");
         setPassword("");
@@ -243,9 +248,16 @@ function LoginWidget({ redirect = true, loginActionPopup, notVerifyHandler }) {
 
         <div className="signin-area mb-3.5">
           <div className="flex justify-center">
-            <GoogleOAuthProvider clientId="780685125249-66b413040g0okik5du7kfp26vhs0vkdc.apps.googleusercontent.com">
+            {/* <GoogleOAuthProvider clientId="780685125249-66b413040g0okik5du7kfp26vhs0vkdc.apps.googleusercontent.com">
               <GoogleLogin onSuccess={socialLogin} onError={errorMessage} />
-            </GoogleOAuthProvider>
+            </GoogleOAuthProvider> */}
+            <GoogleLogin
+              clientId="780685125249-66b413040g0okik5du7kfp26vhs0vkdc.apps.googleusercontent.com"
+              buttonText="Login with Google"
+              onSuccess={socialLogin}
+              onFailure={socialLogin}
+              cookiePolicy={'single_host_origin'}
+            />
           </div>
         </div>
         <div className="signup-area flex justify-center">
